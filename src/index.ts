8d931cee552a5dc392ae699f638151d892dfac33
Steven <3392349+Xapphire13@users.noreply.github.com>
2022-07-21 19:19:38 +0000

    more init

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import THREE from "three";
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+const scene = new THREE.Scene();
+// const camera = new THREE.OrthographicCamera(
+//   width / -2,
+//   width / 2,
+//   height / 2,
+//   height / -2,
+//   1,
+//   1000
+// );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
